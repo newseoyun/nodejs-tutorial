5c2d7bb46eb3367647f0c536aa6e731379f390f2
newseoyun <seoyuntab25@gmail.com>
2019-02-20 15:18:01 +0900

    useQueryId

--- main.js
+++ main.js
@@ -5,30 +5,43 @@ var url = require('url');
 var app = http.createServer(function(request, response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = "it works";
+    var title = queryData.id;
     
     if(_url == '/'){
-        _url = 'index.html';
-        console.log("it works!")
+        console.log("쿼리아이디 정의되지 않음");
+        title = "Welcome!"
+  
      }
     if(_url == '/favicon.ico'){
         return response.writeHead(404);
     }
-    var templete = `
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset="utf-8">
-        </head>
-    
-        <body>
-            <h1>hello hahahaha</h1>
-            <h2>${title}</h2>
-        </body>
-    </html> `
-
 
     response.writeHead(200);
-    response.end(templete);
+
+    fs.readFile(`data/${queryData.id}`,'utf8', function(err, description) {
+        var template = `
+           <!DOCTYPE html>
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <h3>
+                    <ul>
+                        <li><a href="/id?=HTML">HTML</a></li>
+                        <li><a href="/id?=CSS">CSS</a></li>
+                        <li><a href="/id?=JavaScript">JavaScript</a></li>
+                    </ul>
+                    </h3>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </body>
+            </html>`;
+
+        response.end(template);
+    });
+
 });
 app.listen(3000);
